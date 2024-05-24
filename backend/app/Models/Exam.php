0a33575d04c6eb900c5e81fc7435e6817174a737
mohamed mostafa <muhammed.moustafa.ahmed@gmail.com>
2024-05-25 02:03:41 +0300

    make Exam && Question model

--- backend/app/Models/Exam.php
+++ backend/app/Models/Exam.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Exam extends Model
 {
     use HasFactory;
+    protected $fillable = ['exam_name', 'description', 'created_by', 'duration'];
+
+    public function questions(){
+        return $this->hasMany(Question::class);
+    }
+
+    public function creator(){
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function results(){
+        return $this->hasMany(Result::class);
+    }
 }
--- backend/app/Models/Question.php
+++ backend/app/Models/Question.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['exam_id',
+        'question_text',
+        'option_a',
+        'option_b',
+        'option_c',
+        'option_d',
+        'correct_option'
+    ];
+    public function exam(){
+        return $this->belongsTo(Exam::class, 'exam_id');
+    }
 }
