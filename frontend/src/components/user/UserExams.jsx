d7ee6f2b9b7330a6676d1a09ab3701b8b9526da4
TalaatEysa <talaateysa@outlook.com>
2024-06-20 00:37:57 +0300

    modifying UserExams to filter taken exams

--- frontend/src/components/user/UserExams.jsx
+++ frontend/src/components/user/UserExams.jsx
@@ -1,18 +1,30 @@
 import React, { useEffect, useState } from 'react';
-import { getAllExams } from '../../api/axios';
+import { getAllExams, getUserResults } from '../../api/axios';
 import { Link } from 'react-router-dom';
 
 export function UserExams() {
   const [exams, setExams] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  // const token = localStorage.getItem('token');
+  const userId = localStorage.getItem('user_id'); // Adjust based on how you store user info
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await getAllExams();
-        setExams(response.data.data);
+        const [examsResponse, resultsResponse] = await Promise.all([
+          getAllExams(),
+          getUserResults(userId),
+        ]);
+        const examsData = examsResponse.data.data;
+        const userResultsData = resultsResponse.data.data;
+
+        // Filter exams that the user has already taken
+        const availableExams = examsData.filter(
+          (exam) =>
+            !userResultsData.some((result) => result.exam_name === exam.name),
+        );
+
+        setExams(availableExams);
       } catch (error) {
         setError(error.message);
       } finally {
@@ -21,33 +33,34 @@ export function UserExams() {
     };
 
     fetchData();
-  }, []);
+  }, [userId]);
 
-  
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   return (
-    <>
-      <div className="container mt-1">
-        <h1 className="mt-4 mb-5">Exams</h1>
-        {loading && <div>Loading...</div>} {/* Display loading message */}
-        <div className="row">
-          {exams.map((exam) => (
-            <div className="col-md-4 mb-4" key={exam.id}>
-              <div className="card">
-                <div className="card-body">
-                  <h2 className="card-title text-center">{exam.name}</h2>
-                  <p className="card-text">{exam.description}</p>
-                  <Link to={`/userexams/${exam.id}`} className="btn btn-primary">
-                    Take Exam
-                  </Link>
-                </div>
+    <div className="container mt-1">
+      <h1 className="mt-4 mb-5">Exams</h1>
+      <div className="row">
+        {exams.map((exam) => (
+          <div className="col-md-4 mb-4" key={exam.id}>
+            <div className="card">
+              <div className="card-body">
+                <h2 className="card-title text-center">{exam.name}</h2>
+                <p className="card-text">{exam.description}</p>
+                <Link to={`/userexams/${exam.id}`} className="btn btn-primary">
+                  Take Exam
+                </Link>
               </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 }
-
-  
