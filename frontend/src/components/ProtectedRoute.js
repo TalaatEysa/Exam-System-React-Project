229264e1de11250c25a91e3aa617ec189e2241ee
TalaatEysa <talaateysa@outlook.com>
2024-06-21 02:56:06 +0300

    modifying ProtectedRoute

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 
-const ProtectedRoute = ({ element: Component, allowedRoles, ...rest }) => {
+const ProtectedRoute = ({ element: Component, allowedRoles}) => {
     const { user } = useSelector((state) => state.auth);
 
     if (!user) {
         return <Navigate to="/login" />;
     }
-
-    return <Component {...rest} />;
+    return <Component />;
 };
 
 export default ProtectedRoute;
