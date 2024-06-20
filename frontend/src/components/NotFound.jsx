133f7a9eee2dc4d9273cf9a103e1666faed42970
TalaatEysa <talaateysa@outlook.com>
2024-06-20 20:27:42 +0300

    creating not found page

---
+++ frontend/src/components/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function NotFound() {
+  return (
+    <div className="alert alert-danger">
+      <h1 className='text-center'>404 Page Not Found</h1>
+    </div>
+  );
+}
