1432bce23c8c88a76259199f8ecf56ce08dc9f83
TalaatEysa <talaateysa@outlook.com>
2024-06-21 04:23:27 +0300

    creating not authorized component

---
+++ frontend/src/components/NotAuthorized.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export  function NotAuthorized() {
+  return (
+    <div className="alert alert-danger">
+      <h1 className="text-center">403 Not Authorized</h1>
+    </div>
+  );
+}
