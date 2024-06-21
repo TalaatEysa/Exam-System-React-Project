7f0114e2d988d66b173c7c8ac4ba477d4d4d8f50
TalaatEysa <talaateysa@outlook.com>
2024-06-21 23:34:43 +0300

    modifying admin results

--- frontend/src/components/admin/AdminResults.jsx
+++ frontend/src/components/admin/AdminResults.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { getAllResults } from '../../api/axios'; // Adjust the path based on your project structure
+import { getAllExams, getAllResults } from '../../api/axios'; // Adjust the path based on your project structure
 import '../../css/AdminResults.css'; // Import CSS file
 
 const AdminResults = () => {
   const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [exams, setExams] = useState([]);
+  const [loadingExams, setLoadingExams] = useState(true);
+  const [errorExams, setErrorExams] = useState(null);
 
   useEffect(() => {
     const fetchAllResults = async () => {
@@ -18,8 +21,16 @@ const AdminResults = () => {
           setError('Unexpected data format');
         }
         setLoading(false);
+
+        const examsResponse = await getAllExams();
+        setExams(examsResponse.data.data);
+        setLoadingExams(false);
       } catch (error) {
-        setError(error.message || error.response?.data.message || 'Error fetching all results');
+        setError(
+          error.message ||
+            error.response?.data.message ||
+            'Error fetching all results',
+        );
         setLoading(false);
       }
     };
@@ -27,14 +38,15 @@ const AdminResults = () => {
     fetchAllResults();
   }, []);
 
-  if (loading) return <div>Loading...</div>;
+  if (loading || loadingExams) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
-  if (!Array.isArray(results) || results.length === 0) return <div>No results found.</div>;
+  if (!Array.isArray(results) || results.length === 0)
+    return <div>No results found.</div>;
 
   return (
     <div>
-      <h2 className='results-header'>Students' Results</h2>
-      <table className="results-table">
+      <h2 className="results-header">Students' Results</h2>
+      <table className="results-table w-75 mx-auto">
         <thead>
           <tr>
             <th>Student Name</th>
@@ -43,13 +55,29 @@ const AdminResults = () => {
           </tr>
         </thead>
         <tbody>
-          {results.map(result => (
-            <tr key={result.id}>
-              <td>{result.user_name}</td>
-              <td>{result.exam_name}</td>
-              <td>{result.score}</td>
-            </tr>
-          ))}
+          {results.map((result) => {
+            const exam = exams.find((exam) => exam.name === result.exam_name);
+            if (!exam) {
+              // Handle case where exam is not found
+              return (
+                <tr key={result.id}>
+                  <td>{result.user_name}</td>
+                  <td>{result.exam_name}</td>
+                  <td>N/A</td> {/* Provide a default value */}
+                </tr>
+              );
+            }
+            return (
+              <tr key={result.id}>
+                <td>{result.user_name}</td>
+                <td>{result.exam_name}</td>
+                <td>
+                  {result.score}/{exam.questions.length}{' '}
+                  {/* Assuming questions exist */}
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
